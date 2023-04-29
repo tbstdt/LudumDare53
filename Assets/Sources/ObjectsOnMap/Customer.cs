5e0b50e433133097b74ab5fde4964ecfcfa866d9
ivedeneeva <ivedeneeva@gear-games.com>
2023-04-29 18:50:17 +0400

    Получение награды или потеря репутации при выполнении заказа

--- Assets/Sources/ObjectsOnMap/Customer.cs
+++ Assets/Sources/ObjectsOnMap/Customer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using Sources.core;
 using Sources.Editor.ObjectsOnMap;
+using Sources.map;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,7 +15,13 @@ public class Customer : ObjectOnMap
     [SerializeField] private Image m_timerView;
     [SerializeField] private GameObject m_timerGO;
 
+    private OrderGenerator m_orderGenerator;
+
     private int m_timerInSeconds;
+    private bool m_timeout;
+    private bool m_manOnRoad;
+
+    private int m_reputation;
 
     public Order Order { get; private set; }
 
@@ -22,27 +29,18 @@ public class Customer : ObjectOnMap
 
     private void Start()
     {
-        var order = GameCore.Instance.Get<OrderGenerator>().GetOrderData();
+        m_orderGenerator = GameCore.Instance.Get<OrderGenerator>();
 
-        Order = new Order(new Resource(order.Type, order.Amount), order.Reward);
-        m_text.text = order.Amount.ToString();
-        m_timerInSeconds = order.TimeInSeconds;
-
-        if (m_timerInSeconds <= 0)
-        {
-            m_timerGO.SetActive(false);
-            return;
-        }
-
-        StartCoroutine(StartTimer());
+        StartOrder();
     }
 
     private IEnumerator StartTimer()
     {
         m_timerGO.SetActive(true);
+        m_timeout = false;
         float time = m_timerInSeconds;
 
-        while (time > 0)
+        while (time > 0 && !m_timeout)
         {
             time -= Time.deltaTime;
 
@@ -53,15 +51,57 @@ public class Customer : ObjectOnMap
         }
 
         m_timerGO.SetActive(false);
+        OnTimeout();
+    }
+
+    private void OnTimeout()
+    {
+        m_timeout = true;
+        if (!m_manOnRoad)
+        {
+            m_reputation -= Order.ReputationPenalty;
+            Order = null;
+        }
     }
 
     protected override void OnObjectClicked()
     {
+        if (m_timeout)
+            return;
+
         var hub = GameCore.Instance.Get<Hub>();
-        hub.TrySendCourier(this);
+        m_manOnRoad = hub.TrySendCourier(this);
     }
 
     public override void Job(Man man) {
+        if (!m_timeout)
+        {
+            StopCoroutine(StartTimer());
+            m_timeout = true;
+            m_timerGO.SetActive(false);
+        }
 
+        m_manOnRoad = false;
+
+        var hub = GameCore.Instance.Get<Hub>();
+        Resource resource = Order.Reward;
+        GameCore.Instance.Get<MapManager>().LaunchMan(MapPoint, hub, resource);
+    }
+
+    public void StartOrder()
+    {
+        var order = m_orderGenerator.GetOrderData();
+
+        Order = new Order(new Resource(order.Type, order.Amount), order.Reward, order.ReputationReward, order.ReputationPenalty);
+        m_text.text = order.Amount.ToString();
+        m_timerInSeconds = order.TimeInSeconds;
+
+        if (m_timerInSeconds <= 0)
+        {
+            m_timerGO.SetActive(false);
+            return;
+        }
+
+        StartCoroutine(StartTimer());
     }
 }
--- Assets/Sources/Order.cs
+++ Assets/Sources/Order.cs
@@ -1,12 +1,15 @@
 public class Order
 {
     public Resource Resource { get;}
-
     public Resource Reward { get; }
+    public int ReputationReward { get; }
+    public int ReputationPenalty { get; }
 
-    public Order(Resource resource, int rewardAmount)
+    public Order(Resource resource, int rewardAmount, int repReward, int repPenalty)
     {
         Resource = resource;
         Reward = new Resource(ResourceType.Money, rewardAmount);
+        ReputationReward = repReward;
+        ReputationPenalty = repPenalty;
     }
 }
