51316392afd6aa28b92fc656a85029502a52ca51
Anton Mirgasimov <fynjym0@gmail.com>
2023-04-29 13:53:13 +0300

    map generator

--- Assets/Sources/core/GameCore.cs
+++ Assets/Sources/core/GameCore.cs
@@ -5,14 +5,16 @@ namespace Sources.core
 {
 	public class GameCore : MonoBehaviour {
 		[SerializeField] private MapManager _mapManager;
+		[SerializeField] private MapGenerator _mapGenerator;
 		
 		private static CoreManagerProvider _coreManagerProvider;
 
 		public static CoreManagerProvider Instance => _coreManagerProvider ??= new CoreManagerProvider();
 
 		private void Start() {
-				///register
 				Instance.Register<MapManager>(_mapManager);
+				Instance.Register<MapGenerator>(_mapGenerator);
+				
 				Instance.InitManagers();
 		}
 	}
--- Assets/Sources/map/MapGenerator.cs
+++ Assets/Sources/map/MapGenerator.cs
@@ -1,7 +1,25 @@
+using System;
+using System.Collections.Generic;
+using Sources.core;
 using UnityEngine;
 
 namespace Sources.map {
-	public class MapGenerator {
-		[SerializeField] private GameObject _;
+	public class MapGenerator: MonoBehaviour, ICoreRegistrable, ICoreInit {
+		[SerializeField] private List<MapPoint> _points;
+
+		public void Init() {
+			var objectStorage = GameCore.Instance.Get<ObjectsStorage>();
+			foreach (var mapPoint in _points) {
+				var template = objectStorage.GetObjectByType(mapPoint.Type);
+				var objectOnMap = Instantiate(template);
+				objectOnMap.transform.position = mapPoint.Point.transform.position;
+			}
+		}
+	}
+
+	[Serializable]
+	public class MapPoint {
+		public ObjectType Type;
+		public GameObject Point;
 	}
 }
