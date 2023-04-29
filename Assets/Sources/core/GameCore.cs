1dc56fc7dbb550948906ea77b5f9f641679ccc93
Anton Mirgasimov <fynjym0@gmail.com>
2023-04-29 14:07:13 +0300

    update scene

--- Assets/Sources/core/GameCore.cs
+++ Assets/Sources/core/GameCore.cs
@@ -6,6 +6,7 @@ namespace Sources.core
 	public class GameCore : MonoBehaviour {
 		[SerializeField] private MapManager _mapManager;
 		[SerializeField] private MapGenerator _mapGenerator;
+		[SerializeField] private ObjectsStorage _objectsStorage;
 		
 		private static CoreManagerProvider _coreManagerProvider;
 
@@ -14,6 +15,7 @@ namespace Sources.core
 		private void Start() {
 				Instance.Register<MapManager>(_mapManager);
 				Instance.Register<MapGenerator>(_mapGenerator);
+				Instance.Register<ObjectsStorage>(_objectsStorage);
 				
 				Instance.InitManagers();
 		}
--- Assets/Sources/map/MapGenerator.cs
+++ Assets/Sources/map/MapGenerator.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 namespace Sources.map {
 	public class MapGenerator: MonoBehaviour, ICoreRegistrable, ICoreInit {
 		[SerializeField] private List<MapPoint> _points;
+		[SerializeField] private Transform _container;
 
 		public void Init() {
 			var objectStorage = GameCore.Instance.Get<ObjectsStorage>();
 			foreach (var mapPoint in _points) {
-				var template = objectStorage.GetObjectByType(mapPoint.Type);
-				var objectOnMap = Instantiate(template);
+				var objectOnMap = objectStorage.GetObjectByType(mapPoint.Type);
+				objectOnMap.transform.SetParent(_container);
 				objectOnMap.transform.position = mapPoint.Point.transform.position;
 			}
 		}
