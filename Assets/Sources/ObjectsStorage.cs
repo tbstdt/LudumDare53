33b35324735d495a40eba8e84226357324b89b12
ivedeneeva <ivedeneeva@gear-games.com>
2023-04-29 14:40:42 +0400

    Хранилище объектов для создания объекта по типу из префаба

--- Assets/Sources/ObjectsStorage.cs
+++ Assets/Sources/ObjectsStorage.cs
@@ -1,9 +1,22 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Sources.core;
 using UnityEngine;
 
-public class ObjectsStorage : ICoreRegistrable
+public class ObjectsStorage : MonoBehaviour, ICoreRegistrable
 {
-    private Dictionary<ObjectType, ObjectOnMap> m_objectsbyType = new Dictionary<ObjectType, ObjectOnMap>();
+    [SerializeField] private List<ObjectPrefabStruct> m_prefabs;
+
+    public ObjectOnMap GetObjectByType(ObjectType type)
+    {
+        return Instantiate(m_prefabs.First(o => o.ObjectType == type).ObjectPrefab);
+    }
+}
+
+[Serializable]
+public struct ObjectPrefabStruct
+{
+    public ObjectType ObjectType;
+    public ObjectOnMap ObjectPrefab;
 }
